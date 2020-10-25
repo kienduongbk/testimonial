245ed7a703a6cc5fa3d40816635c9d344204890a
kiendt <kienduong.hust@gmail.com>
2020-10-25 10:19:02 +0700

    update layout default

--- tmpl/default.php
+++ tmpl/default.php
@@ -17,28 +17,26 @@ $col = $params->get('col');
 ?>
 <div id="js-testimonial" class="js-section" role="testimonial">
     <div class="js-testimonial-inner container">
-        <div class="custom">
-            <div class="section-heading text-center">
-                <h2>Testimonials</h2>
+        <div class="section-heading text-center">
+            <h2>Testimonials</h2>
 
-                <p>What customers say about us</p>
-            </div>
-
-            <ul class="testimonial-list row">
-                <?php foreach ($data as $item) : ?>
-                    <li class="item col-md-4">
-                        <div class="testimonial-block">
-                            <img src="<?php echo $item->testimonial_img; ?>" alt="Sample image" class="img-circle" />
-                            <h4 class="author-name"><?php echo $item->testimonial_name; ?></h4>
-                            <h4 class="author-position"><?php echo $item->testimonial_position; ?></h4>
-
-                            <p>
-                                <?php echo $item->testimonial_description; ?>
-                            </p>
-                        </div>
-                    </li>
-                <?php endforeach; ?>
-            </ul>
+            <p>What customers say about us</p>
         </div>
+
+        <ul class="testimonial-list row">
+            <?php foreach ($data as $item) : ?>
+                <li class="item col-md-4">
+                    <div class="testimonial-block">
+                        <img src="<?php echo $item->testimonial_img; ?>" alt="Sample image" class="img-circle" />
+                        <h4 class="author-name"><?php echo $item->testimonial_name; ?></h4>
+                        <h4 class="author-position"><?php echo $item->testimonial_position; ?></h4>
+
+                        <p>
+                            <?php echo $item->testimonial_description; ?>
+                        </p>
+                    </div>
+                </li>
+            <?php endforeach; ?>
+        </ul>
     </div>
 </div>
